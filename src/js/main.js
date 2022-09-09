0463f71ba20b9f2fe934ce0a8a7e001be88d7ac9
Letícia Silva <leticiati021@gmail.com>
2022-09-09 13:35:00 -0400

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { typeWrite } from "./typeWrite.js";
+
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+
